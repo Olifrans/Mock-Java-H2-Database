eba13ce273b28984dd323ae701fca9680b21f9e4
Francisco Oliveira <fransoliveira@gmail.com>
2025-05-13 21:50:31 -0300

    final

--- src/main/java/com/aprende/oliveira/OliveiraApplication.java
+++ src/main/java/com/aprende/oliveira/OliveiraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OliveiraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OliveiraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OliveiraApplication.class, args);
+    }
 
 }
